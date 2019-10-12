038c5153ee2427f3292009c731b4648a84d3c09c
milindjayan <41206597+milindjayan@users.noreply.github.com>
2019-10-11 21:07:44 -0400

    updated the getNodes function
    Updated the genNOdes function to generate hexadecimal numbers based on the numNodes recieved as input

--- project_3/lib/tapestryAPI.ex
+++ project_3/lib/tapestryAPI.ex
@@ -15,8 +15,20 @@ defmodule TapestryAPI do
     hashid_pid_map
   end
 
-  def getNodeIds(_numNodes) do
-    [1234,2345,3456,4567,5678,6789,7890,8901,9012]
+  def getNodeIds(numNodes) do
+    numNodes_up=if rem(numNodes,16) != 0,do: (numNodes-rem(numNodes,16))+16,else: numNodes
+    max_length = length(Integer.digits(numNodes_up))
+    return_nodes = for i<- 1..numNodes_up do
+      hex_i = Integer.to_string(i,16)
+      hex_i_len = String.length(hex_i)
+      if hex_i_len<max_length do
+        zeroes=String.duplicate("0",max_length-hex_i_len)
+        zeroes<>hex_i
+      else
+        hex_i
+      end
+    end
+    Enum.shuffle(Enum.slice(return_nodes,0,numNodes))
   end
 
   def assignHash(node_ids) do
