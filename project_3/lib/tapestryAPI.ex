3f74f8ad59e9bff51de281487f397af234d0759a
milindjayan <41206597+milindjayan@users.noreply.github.com>
2019-10-15 16:09:18 -0400

    Update tapestryAPI.ex

--- project_3/lib/tapestryAPI.ex
+++ project_3/lib/tapestryAPI.ex
@@ -8,15 +8,19 @@ defmodule TapestryAPI do
 
   def startTapestry(numNodes) do
     #get the node ids based on number of nodes
-    node_ids = getNodeIds(numNodes)
+    {sorted_node_ids,unsorted_node_ids} = getNodeIds(numNodes)
 
     #assign nodeids to pids
-    hashid_pid_map = assignHash(node_ids)
-    hashid_pid_map
+    hashid_pid_map = assignHash(unsorted_node_ids)
+
+    #create the network
+    createNetwork(sorted_node_ids,hashid_pid_map)
+
+
   end
 
   def getNodeIds(numNodes) do
-    numNodes_up=if rem(numNodes,16) != 0,do: (numNodes-rem(numNodes,16))+16,else: numNodes
+    numNodes_up= :math.pow(16,Float.ceil(:math.log(numNodes)/:math.log(16)))|>round
     max_length = length(Integer.digits(numNodes_up))
     return_nodes = for i<- 1..numNodes_up do
       hex_i = Integer.to_string(i,16)
@@ -28,7 +32,18 @@ defmodule TapestryAPI do
         hex_i
       end
     end
-    Enum.shuffle(Enum.slice(return_nodes,0,numNodes))
+    shuffled = Enum.shuffle(return_nodes)
+    unsorted_nodeIds = Enum.slice(shuffled,0,numNodes)
+
+    int_hex_map = Enum.reduce(unsorted_nodeIds,%{},fn (nodeId,acc_int_hex) ->
+      {in_for_hex,""}= Integer.parse(nodeId, 16)
+      int_hex_map = Map.put(acc_int_hex,nodeId,in_for_hex)
+      int_hex_map
+    end)
+
+    sorted_nodeIds = Enum.sort(unsorted_nodeIds,fn (a,b) -> int_hex_map[a]<int_hex_map[b] end)
+
+    {sorted_nodeIds,unsorted_nodeIds}
   end
 
   def assignHash(node_ids) do
@@ -37,6 +52,44 @@ defmodule TapestryAPI do
       Map.put(return_map,hash,pid)
     end)
     has_pid_map
+  end
+
+  def createNetwork(sorted_node_ids,hashid_pid_map) do
+
+  end
+
+  def level(node1, node2) do
+    # node1 = identifier for node 1
+    # node2 = idenitfier for node 2
+    # The levels start from L1 to L4
+    level = 0;
+    # node1 = Integer.to_string(node1_int);
+    # node2 = Integer.to_string(node2_int);
+    length = String.length(node1);
+    #IO.inspect(level);
+    currentPos = 0;
+    level = levelFind(node1, node2, level, currentPos, length);
+    # if(level >= length) do
+    #   level = length;
+    # end
+    level = level + 1;
+    level
+  end
+
+  def levelFind(node1, node2, level, currentPos, length) do
+    if(String.at(node1, currentPos) == String.at(node2, currentPos)) do
+      #IO.inspect(String.at(node1, currentPos));
+      level = level + 1;
+      currentPos = currentPos + 1
+      if(currentPos < length) do
+        #IO.inspect(level)
+        levelFind(node1, node2, level, currentPos, length)
+      else
+        level
+      end
+    else
+      level
+    end
   end
 
 
