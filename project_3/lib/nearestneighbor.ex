f9c3ae6b24e008302cc0b36e1218bce9c1e12e81
advaitambeskar <ambeskaradvait@gmail.com>
2019-10-20 17:06:37 -0400

    Update nearestneighbor.ex

--- project_3/lib/nearestneighbor.ex
+++ project_3/lib/nearestneighbor.ex
@@ -4,7 +4,6 @@ defmodule NearestNeighbor do
     # convert the hexadecimal 'node' to integer?
     # sorted_map => sorted integer map -> convert the answer into hexadecimal when returning
     # node => hexadecimal value of string
-
     node_int = String.to_integer(node, 16);
     # now that we have the integer form of the given hexademical number, we can use it to compare to
     # the lookup table and find the nearest neighbor
@@ -13,38 +12,56 @@ defmodule NearestNeighbor do
     sorted_map_int = Enum.map(
       sorted_map, fn x -> String.to_integer(x, 16) end
     )
-    closestNeighbor = near_finder(node_int, sorted_map_int, length, index)
+    # IO.inspect(node_int)
+    # IO.inspect(sorted_map_int)
+    closestNeighbor = nil
+    closestNeighbor = near_finder(node_int, sorted_map_int, length, index, closestNeighbor, false)
 
     closestNeighbor
   end
 
-  def near_finder(node, sorted_map, length, index) do
-    if(index >= length) do
-      %{}
-    else
-      currentValue = sorted_map.at(index)
-      if(node <= currentValue) do
-        # This is where we pass the function to find the closest neighbor
-        if(index != 0) do
-          currentdifference = abs(node - currentValue)
-          previousvalue = sorted_map.at(index - 1)
-          previousdifference = abs(node - previousvalue)
-          if(currentdifference <= previousdifference) do
-            #the current value is closest neighbor. so we return currentvalue in hex
-            # this needs to be returned
-            closest_neighbor = Integer.to_string(currentValue, 16)
+  def near_finder(node, sorted_map, length, index, closest_neighbor, is_closest_found) do
+    if(is_closest_found == false) do
+      if(index > length-1) do
+        closest_neighbor = Enum.at(sorted_map, index-1)
+        near_finder(node, sorted_map, length, index, closest_neighbor, true)
+      else
+        currentValue = Enum.at(sorted_map, index)
+        # IO.inspect sorted_map
+        # IO.inspect("What is #{currentValue}")
+        # IO.inspect("node is #{node}")
+        # IO.inspect("index is #{index}")
+        # IO.inspect length
+        if(node <= currentValue) do
+          # IO.inspect("here")
+          # This is where we pass the function to find the closest neighbor
+          if(index != 0) do
+            currentdifference = abs(node - currentValue)
+            previousvalue = Enum.at(sorted_map, index - 1)
+            previousdifference = abs(node - previousvalue)
+            if(currentdifference <= previousdifference) do
+              #the current value is closest neighbor. so we return currentvalue in hex
+              # this needs to be returned
+              closest_neighbor = Integer.to_string(currentValue, 16)
+              near_finder(node, sorted_map, length, index, closest_neighbor, true)
+            else
+              # this needs to be returned
+              closest_neighbor = Integer.to_string(previousvalue, 16)
+              near_finder(node, sorted_map, length, index, closest_neighbor, true)
+            end
           else
             # this needs to be returned
-            closest_neighbor = Integer.to_string(previousvalue, 16)
+            closest_neighbor = Integer.to_string(currentValue, 16)
+            near_finder(node, sorted_map, length, index, closest_neighbor, true)
           end
         else
-          # this needs to be returned
-          closest_neighbor = Integer.to_string(currentValue, 16)
+          # IO.inspect("here where #{node} is greater than #{currentValue}")
+          index = index + 1
+          near_finder(node, sorted_map, length, index, closest_neighbor, is_closest_found)
         end
-      else
-        index++
-        near_finder(node, sorted_map, length, index)
       end
+    else
+      closest_neighbor
     end
   end
 end
