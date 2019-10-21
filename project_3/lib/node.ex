4f8e1f23682d35a7ca0b92285806a39b6a7ac7f1
milindjayan <41206597+milindjayan@users.noreply.github.com>
2019-10-21 02:33:39 -0400

    Created the routing table statically

--- project_3/lib/node.ex
+++ project_3/lib/node.ex
@@ -1,13 +1,17 @@
 defmodule CheckNode do
   use GenServer
 
-  def start(hash) do
-    {:ok,pid} = GenServer.start(__MODULE__,hash)
-    {hash,pid}
+  def start(node_id) do
+    {:ok,pid} = GenServer.start(__MODULE__,node_id)
+    {node_id,pid}
   end
 
   def get_state(pid) do
     GenServer.call(pid,:getstate)
+  end
+
+  def update_state(pid,routing_table) do
+    GenServer.cast(pid,{:updateState,routing_table})
   end
 
 
@@ -16,8 +20,15 @@ defmodule CheckNode do
     {:reply,state,state}
   end
 
-  def init(hash) do
-    {:ok,%{:nodeID=>hash}}
+  def handle_cast({:updateState,routing_table},state_map) do
+    state_map = Map.put(state_map,:routing_table , routing_table)
+    {:noreply,state_map}
+  end
+
+
+
+  def init(node_id) do
+    # init_routing_table = create_initial_routing_table(node_id)
+    {:ok,%{:nodeID=>node_id}}
   end
 end
-
--- project_3/lib/tapestryAPI.ex
+++ project_3/lib/tapestryAPI.ex
@@ -7,17 +7,19 @@ defmodule TapestryAPI do
 
 
   def startTapestry(numNodes) do
+
     #get the node ids based on number of nodes
     {sorted_node_ids,unsorted_node_ids} = getNodeIds(numNodes)
 
     #assign nodeids to pids
-    hashid_pid_map = assignHash(unsorted_node_ids)
+    nodeid_pid_map = assignHash(unsorted_node_ids)
 
     #create the network
-    # createNetwork(sorted_node_ids,unsorted_node_ids,hashid_pid_map)
-    {sorted_node_ids,unsorted_node_ids,hashid_pid_map}
- end
+    create_network(sorted_node_ids,unsorted_node_ids,nodeid_pid_map)
 
+  end
+
+  # generate the node ids
   def getNodeIds(numNodes) do
     # numNodes_up= :math.pow(16,Float.ceil(:math.log(numNodes)/:math.log(16)))|>round
     unsorted_nodeIds = generate_node_id(numNodes,[])
@@ -55,6 +57,7 @@ defmodule TapestryAPI do
     Enum.join(hash_val)
   end
 
+  # start the nodes and assign hashes
   def assignHash(node_ids) do
     has_pid_map = Enum.reduce(node_ids,%{},fn (hash,return_map) ->
       {hash,pid} = CheckNode.start(hash)
@@ -63,6 +66,9 @@ defmodule TapestryAPI do
     end)
     has_pid_map
   end
+
+
+  # create the network
 
   def create_initial_routing_table(node_id) do
     route_table = %{1 => {nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil},
@@ -89,11 +95,76 @@ defmodule TapestryAPI do
     join_elems(node_id_list,n_level+1,route_table,node_id)
   end
 
-  def createNetwork(sorted_node_ids,hashid_pid_map) do
-      IO.inspect "None"
+  def create_network(sorted_node_ids,unsorted_node_ids,nodeid_pid_map) do
+    Enum.each(sorted_node_ids, fn(node_id) ->
+      complete_route_table(node_id,nodeid_pid_map,sorted_node_ids)
+    end)
   end
 
-  def level(node1, node2) do
+  def complete_route_table(node_id,nodeid_pid_map,sorted_node_ids) do
+    init_routing_table = create_initial_routing_table(node_id)
+
+    routing_table = Enum.reduce(sorted_node_ids, init_routing_table, fn (check_node_id,routing_table_acc) -> (
+      cond do
+        String.equivalent?(node_id,check_node_id) == false ->
+          # IO.inspect "Check Node ID"
+          # IO.inspect check_node_id
+          {level,col_pos} = find_level_entry(node_id,check_node_id)
+          # IO.inspect level
+          # IO.inspect col_pos
+          col_pos = String.to_integer(col_pos,16)
+          # IO.inspect(col_pos)
+          row_vals = routing_table_acc[level]
+          # IO.inspect row_vals
+          val_at_point = elem(row_vals,col_pos)
+          # IO.inspect val_at_point
+          return_val =
+            cond do
+              val_at_point == nil ->
+                check_node_id
+              true ->
+                # IO.puts "val_at_point"
+                # IO.puts val_at_point
+                # IO.puts "node_id"
+                # IO.inspect node_id
+                # IO.puts "check_node_id"
+                IO.inspect check_node_id
+                find_difference(node_id,val_at_point,check_node_id)
+            end
+
+          row_vals = put_elem(row_vals,col_pos,return_val)
+          routing_table_acc = Map.put(routing_table_acc,level,row_vals)
+        true->
+          routing_table_acc
+      end)
+    end)
+    detination_pid = nodeid_pid_map[node_id]
+    CheckNode.update_state(detination_pid,routing_table)
+  end
+
+
+  def find_level_entry(node1,node2) do
+      level = find_level(node1,node2)
+      entry = Enum.at(String.graphemes(node2),level-1)
+      {level,entry}
+  end
+
+  def find_difference(main_node,check_node1,check_node2) do
+    main_node = String.to_integer(main_node,16)
+    check_node1_int = String.to_integer(check_node1,16)
+    check_node2_int = String.to_integer(check_node2,16)
+
+    diff1 = abs(main_node - check_node1_int)
+    diff2 = abs(main_node - check_node2_int)
+
+    if diff1 < diff2 do
+      check_node1
+    else
+      check_node2
+    end
+  end
+
+  def find_level(node1, node2) do
     # node1 = identifier for node 1
     # node2 = idenitfier for node 2
     # The levels start from L1 to L4
@@ -103,12 +174,32 @@ defmodule TapestryAPI do
     length = String.length(node1);
     #IO.inspect(level);
     currentPos = 0;
+    # node1 = removeZeroes(node1)
+    # node2 = removeZeroes(node2)
+
+    if(String.length(node1) > String.length(node2)) do
+      addZeroes(node2, String.length(node1) - String.length(node2))
+    else
+      addZeroes(node1, String.length(node2) - String.length(node1))
+    end
+
+
     level = levelFind(node1, node2, level, currentPos, length);
     # if(level >= length) do
     #   level = length;
     # end
     level = level + 1;
     level
+  end
+
+  def removeZeroes(node) do
+    # remove zeroes from the start of the node
+    String.replace_leading(node, "0", "");
+  end
+
+  def addZeroes(node, numberOfAdditions) do
+    # add zeroes equal to the numberOfAdditions to the beginning of the list.
+    String.pad_leading(node, numberOfAdditions, "0");
   end
 
   def levelFind(node1, node2, level, currentPos, length) do
